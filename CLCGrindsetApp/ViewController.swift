d26e5bc68f1e46530dbd8d71fd06f8d209d9664c
esnoftz <esnoftz@gmail.com>
2025-04-15 07:59:36 -0500

    Initial Commit

---
+++ CLCGrindsetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLCGrindsetApp
+//
+//  Created by EVANGELINE NOFTZ on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CLCGrindsetAppTests/CLCGrindsetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CLCGrindsetAppTests.swift
+//  CLCGrindsetAppTests
+//
+//  Created by EVANGELINE NOFTZ on 4/15/25.
+//
+
+import Testing
+@testable import CLCGrindsetApp
+
+struct CLCGrindsetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
