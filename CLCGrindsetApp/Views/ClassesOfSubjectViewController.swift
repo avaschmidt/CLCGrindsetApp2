3f1fe11a0691a960992a5363734acf5c15c5ee6d
Ryan <ryan@starkfamily.org>
2025-04-17 13:33:14 -0500

    //    my name is ryan and i smell

---
+++ CLCGrindsetApp/Views/ClassesOfSubjectViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ClassesOfSubjectViewController.swift
+//  CLCGrindsetApp
+//
+//  Created by RYAN STARK on 4/17/25.
+//
+
+import UIKit
+
+class ClassesOfSubjectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+//    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+    //    my name is ryan and i smell
+     
+}
