7901b033161fa08b236282ab7da5c56ac9ac210b
esnoftz <esnoftz@gmail.com>
2025-04-21 08:44:24 -0500

    orange vc new.

--- CLCGrindsetApp/Views/MakeScheduleViewController.swift
+++ CLCGrindsetApp/Views/MakeScheduleViewController.swift
@@ -4,7 +4,38 @@ import UIKit
 
 class MakeScheduleViewController: UIViewController {
    
-   
+    @IBAction func edit1Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit2Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit3Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit4Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit5Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit6Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit7Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit8Action(_ sender: UIButton) {
+    }
+    
+    @IBAction func edit9Action(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
     
 
 }
