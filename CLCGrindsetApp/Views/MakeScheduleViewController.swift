b9ec0f834ceadbd2e8902c7ed25e52206b5f102c
esnoftz <esnoftz@gmail.com>
2025-04-22 07:36:48 -0500

    segues

--- CLCGrindsetApp/Views/MakeScheduleViewController.swift
+++ CLCGrindsetApp/Views/MakeScheduleViewController.swift
@@ -5,30 +5,39 @@ import UIKit
 class MakeScheduleViewController: UIViewController {
    
     @IBAction func edit1Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit2Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit3Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit4Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit5Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit6Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit7Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit8Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     @IBAction func edit9Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "showSubjects", sender: nil)
     }
     
     
