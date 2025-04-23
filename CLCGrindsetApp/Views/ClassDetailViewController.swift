f858b72f352054c6385e45cce9e12bd5b9862f36
Ryan <ryan@starkfamily.org>
2025-04-23 07:40:08 -0500

    details and stuff added

---
+++ CLCGrindsetApp/Views/ClassDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ClassDetailViewController.swift
+//  CLCGrindsetApp
+//
+//  Created by RYAN STARK on 4/23/25.
+//
+
+import UIKit
+
+class ClassDetailViewController: UIViewController  {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var creditLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
