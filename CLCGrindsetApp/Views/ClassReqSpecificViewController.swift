7360529eb34afd6c38edcfe72369ddea1d3c4d1c
gerald <matthew.fitch26@d155.org>
2025-04-23 06:08:18 -0700

    add back the classes x code deleted. sorry your code won't run because main isn't fixed :D

--- CLCGrindsetApp/Views/ClassReqSpecificViewController.swift
+++ CLCGrindsetApp/Views/ClassReqSpecificViewController.swift
@@ -4,27 +4,53 @@
 //
 //  Created by EVAN MEYER on 4/21/25.
 //
-
 import UIKit
-
-class ClassReqSpecificViewController: UIViewController {
-
-    @IBOutlet weak var tableviewOutlet: UITableView!
+class ClassReqSpecificViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        sortedClasses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.row
+        
+        var cell = classesTableView.dequeueReusableCell(withIdentifier: "ClassReqCell") as! ReqCell
+        
+        cell.customize(name: sortedClasses[index].courseName, creditText: "\(sortedClasses[index].credits) credits")
+        
+        return cell
+    }
+    
+    let classes = ["English", "Mathematics", "Science", "Computer science", "Drivers Ed", "Consumer Ed", "PE / Gym", "Social Science", "Electives"]
+    
+    let countAs = [["English"], ["Math"], ["Science"], ["Computer Science"], ["Driver's Education"], ["Family and Consumer Science"], ["Wellness"], ["Social Science"], ["Art", "Music", "World Language", "Business", "Industrial Technology", "Work Program"]]
+    @IBOutlet weak var classesTableView: UITableView!
+    
+    var sortedClasses = [Course]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var curCreditType = AppData.reqType
+        
+        hello: for course in allCourses {
+            for specificType in countAs[curCreditType] {
+                if course.subject == specificType {
+                    sortedClasses.append(course)
+                    
+                    continue hello
+                }
+            }
+        }
+        
+        classesTableView.dataSource = self
+        classesTableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
-
-}
