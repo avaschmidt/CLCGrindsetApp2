5ec1f133daf7af9b6753aa123966046ad0164cf7
gerald <matthew.fitch26@d155.org>
2025-04-28 06:48:52 -0700

    Logout button do worky.

---
+++ CLCGrindsetApp/Views/LogoutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LogoutViewController.swift
+//  CLCGrindsetApp
+//
+//  Created by MATTHEW FITCH on 4/28/25.
+//
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+
+    @IBOutlet weak var areYouLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        areYouLabel.text = "User logged in: \((AppData.currentStudent!).username!)\nAre You A..."
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutAction(_ sender: Any) {
+        AppData.currentStudent = Student(dict: ["": ""])
+        AppData.saveUserAndPass()
+        self.navigationController?.popViewController(animated: false)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
